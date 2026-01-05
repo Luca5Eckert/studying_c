3405c6a7c938e853a6b831fdbf7a95cdcda5a88d
Luca5Eckert <ecklukatv@gmail.com>
2026-01-05 14:28:05 -0300

    feat: add the function init to dynamic array

--- pointer/dynamic_array.c
+++ pointer/dynamic_array.c
@@ -8,7 +8,27 @@ typedef struct {
     size_t capacity;
 } Numbers;
 
-void add(Numbers *nums, int num) {
+int init(Numbers **nums_copy, size_t capacity) {
+    Numbers *nums = malloc(sizeof(Numbers));
+    if (!nums) {
+        return -1;
+    }
+
+    nums->itens = malloc(sizeof(int) * capacity);
+    if (!nums->itens) {
+        free(nums);
+        return -1;
+    }
+
+    nums->capacity = capacity;
+    nums->size = 0;
+
+    *nums_copy = nums;
+
+    return 0;
+}
+
+void add(Numbers *nums, const int num) {
     do {
         if (nums->size >= nums->capacity) {
             nums->capacity *= 2;
@@ -16,7 +36,7 @@ void add(Numbers *nums, int num) {
             int *temp = realloc(nums->itens, nums->capacity * sizeof(int));
 
             if (!temp) {
-                printf("Erro ao alocar mémoria: Não foi possível alocar mémoria para a lista");
+                printf("Memory allocation failed\n");
                 return;
             }
 
@@ -28,5 +48,26 @@ void add(Numbers *nums, int num) {
 
     } while (0);
 }
+
+int main() {
+    Numbers *nums;
+
+    const int result = init(&nums, 5);
+    if (result != 0) {
+        return -1;
+    }
+
+    add(nums, 1);
+
+    for (int i = 0; i < nums->size; i++) {
+        printf("%d\n", nums->itens[i]);
+    }
+
+    free(nums);
+    free(nums->itens);
+
+    return 0;
+}
+
 
 
